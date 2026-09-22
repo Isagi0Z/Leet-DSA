97b633fd4a4615e80941b41df4299dabf8792dab
Ratish S A <sratish2023@gmail.com>
2026-09-22 11:59:58 +0530

    Time: 3 ms (16.98%), Space: 11.6 MB (5.2%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
     int reverseDegree(string s) {
+        unordered_map<char,int> mp;
+        int x=26;
+        for(char c='a'; c<='z'; c++){
+            mp[c]=x;
+            x--;
+        }
         int ans=0;
 
         for (int i=0; i<s.size(); i++){
             char c=s[i];
-            ans+=('z'-c+1)*(i+1);
+            ans+=(mp[c])*(i+1);
         }
         return ans;
     }
